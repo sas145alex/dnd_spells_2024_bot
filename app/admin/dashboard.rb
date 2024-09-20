9fdaab0fb836aa20cf504bd0bdd79d32e5e15000
aleksei saz <sas145work@gmail.com>
2024-09-20 16:08:03 +0400

    Show stattistics in dasboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,31 +4,29 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    div class: "row" do
+      div class: "col-sm-6" do
+        panel "Most requested spells" do
+          table_for Spell.order(requested_count: :desc).limit(10) do |t|
+            t.column :title do |spell|
+              link_to(spell.title, admin_spell_path(spell))
+            end
+            t.column :requested_count
+          end
+        end
+      end
+
+      div class: "col-sm-6" do
+        panel "Recent users" do
+          table_for TelegramUser.order(last_seen_at: :desc).limit(10) do |t|
+            t.column :id do |user|
+              user.external_id
+            end
+            t.column :last_seen_at
+            t.column :spells_requested_count
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
